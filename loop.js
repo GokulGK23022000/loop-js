5a2a9acfcfbd68831a420db3bc06481d0a8ad991
gokulgk232200 <dgokulgk@gmail.com>
2023-08-04 13:29:53 +0530

    new

---
+++ loop.js
@@ -0,0 +1,55 @@
+ // 1.	Print N numbers in reverse order
+let n=parseInt(prompt("enter your no:"));
+console.log("Print N numbers in reverse order")
+for(let i=n;i>=1;i--){
+    console.log(i)
+}
+
+
+// 2.Print N odd numbers
+let a=parseInt(prompt("Enter a number"));
+console.log("Print N odd numbers")
+for (let i=1;i<=a;i++){
+    if ((i%2)!=0){
+        console.log(i)
+    }
+}
+
+
+// 3.Calculate sum of a number
+let b=parseInt(prompt("enter a number"));
+console.log("Calculate sum of a number")
+let sum=0;
+for (let i=1;i<=b;i++){
+    sum=sum +i;
+}
+console.log(sum)
+
+
+// 4.Print Table of given number
+let c=parseInt(prompt('Enter a number: '));
+console.log("Print Table of given number")
+for (let i=1;i<=10;i++){
+    mul=c*i;
+    console.log(`${c}*${i}=${mul}`)
+}
+
+
+// 5.Check number is prime or not
+let d = parseInt(prompt("Enter a number: "));
+console.log("Check number is prime or not");
+
+let flag = true;
+
+for (let i = 2; i <= d/2;i++) {
+  if (d % i == 0) {
+    flag = false;
+    break;
+  }
+}
+
+if (flag) {
+  console.log('Prime number')
+} else {
+  console.log('Not a prime number');
+}
